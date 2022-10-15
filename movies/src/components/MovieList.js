7faf217284ec1485e9ef3ee20318e2e724dfc324
AHMET UCAR <213214006@stu.adu.edu.tr>
2022-10-15 16:42:13 +0300

    Update MovieList.js

--- movies/src/components/MovieList.js
+++ movies/src/components/MovieList.js
@@ -13,7 +13,7 @@ const MovieList = (props) => {
                  <div className="card mb-4 shadow-sm">
                    <img src= {movie.imageURL} className="card-img-top img-thumbnail" alt="Sample Movie" ></img>
                    <div className="card-body">
-                       <h5 className="card-title"> {movie.name} </h5>
+                       <h5 className="card-title"> {movie.title} </h5>
                        <p className="card-text"> {movie.overview} </p>
                        <div className="d-flex justify-content-between align-items-center">
                           <button type="button" className="btn btn-md btn-outline-danger">Delete</button>
